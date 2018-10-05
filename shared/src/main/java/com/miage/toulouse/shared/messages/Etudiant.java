0ae8192cdf20e4a8c3f051b6287b7f8c4cf4e14d
mh0 <mahdi.hentati@gmail.com>
2018-10-05 22:35:15 +0200

    projet serveur etudiant

---
+++ shared/src/main/java/com/miage/toulouse/shared/messages/Etudiant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miage.toulouse.shared.messages;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class Etudiant {
+    public Long numero;
+    public String nom;
+    public String prenom;
+}
