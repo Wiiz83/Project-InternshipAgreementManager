5bae608225a16f4f347392ea361754cb8b3f742e
mh0 <mahdi.hentati@gmail.com>
2018-10-07 06:31:42 +0200

    ajout du script de dépoloiement

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+typeset -A config 
+
+. ./path.cfg
+
+githubrepo=${PWD}
+
+cd ${config[galssfish_path]}
+cd ./bin/
+./asadmin delete-jms-host default_JMS_host
+./asadmin create-jms-host --mqhost localhost --mqport 56421 --mquser jmsclient --mqpassword 123456789 jmsuniversite
+./asadmin set server.jms-service.type=REMOTE
+./asadmin set server.jms-service.default-jms-host=jmsuniversite
+
+cat ${githubrepo}/gfressources.cfg | while read gfr
+  do  
+  ./asadmin add-resources ${githubrepo}$gfr
+  done 
+
+cd ${config[openmq_path]}
+#./mq/bin/imqusermgr update -u admin -a true -p 123456789
+./mq/bin/imqusermgr add -u jmsclient -p 123456789
+./mq/bin/imqbrokerd -port 56421
