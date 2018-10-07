890d0722aa787bc086c9c608b901788905493e43
Lucas <uzanlah@gmail.com>
2018-10-07 15:31:12 +0200

    Exemple TP Bourse

---
+++ Exemples/TPBourse/src/tpbourse/Receiver.java
@@ -0,0 +1,95 @@
+package tpbourse;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Destination;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
+import javax.jms.MessageConsumer;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+
+public class Receiver {
+
+    /**
+     * Main line.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "jms/__defaultConnectionFactory";
+        String destName = "Bourse";
+        Destination dest = null;
+        int count = 1;
+        Session session = null;
+        MessageConsumer receiver = null;
+
+        try {
+            // create the JNDI initial context
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the receiver
+            receiver = session.createConsumer(dest, "JMSType IN ('T1','T2')");
+
+            // start the connection, to enable message receipt
+            connection.start();
+
+            for (int i = 0; i < count; ++i) {
+                Message message = receiver.receive();
+                
+              
+                
+                if (message instanceof ObjectMessage) {
+                    ObjectMessage object = (ObjectMessage) message;
+                    TitreBoursier titre = (TitreBoursier) object.getObject();
+                    System.out.println("Received: " + titre.getNom().toString());
+                } else if (message != null) {
+                    System.out.println("Received non text message");
+                }
+            }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+}
