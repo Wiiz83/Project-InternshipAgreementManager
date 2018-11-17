e54cbf16bb5359e4b54af57db65fa68a354252f7
mh0 <mahdi.hentati@gmail.com>
2018-11-17 20:56:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ cat "${githubrepo}/gfressources.cfg" | while read gfr
   
 # JMS REMOTE:
 #./asadmin delete-jms-host default_JMS_host
-#./asadmin create-jms-host --mqhost localhost --mqport 56421 --mquser jmsclient --mqpassword 123456789 jmsuniversite
-#./asadmin set server.jms-service.type=REMOTE
-#./asadmin set server.jms-service.default-jms-host=jmsuniversite
+./asadmin create-jms-host --mqhost localhost --mqport 56421 --mquser jmsclient --mqpassword 123456789 jmsuniversite
+./asadmin set server.jms-service.type=REMOTE
+./asadmin set server.jms-service.default-jms-host=jmsuniversite
 
-#cd "${config[openmq_path]}"
-#./mq/bin/imqusermgr update -u admin -a true -p 123456789
-#./mq/bin/imqusermgr add -u jmsclient -p 123456789
+cd "${config[openmq_path]}"
+./mq/bin/imqusermgr update -u admin -a true -p 123456789
+./mq/bin/imqusermgr add -u jmsclient -p 123456789
