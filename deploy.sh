12cb78b920cbe964debc8cf78a9d26141453443d
Mahdi <mh0@users.noreply.github.com>
2018-11-17 21:43:22 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ cat "${githubrepo}/gfressources.cfg" | while read gfr
   done 
   
 # JMS REMOTE:
-#./asadmin delete-jms-host default_JMS_host
+./asadmin delete-jms-host default_JMS_host
 ./asadmin create-jms-host --mqhost localhost --mqport 56421 --mquser jmsclient --mqpassword 123456789 jmsuniversite
 ./asadmin set server.jms-service.type=REMOTE
 ./asadmin set server.jms-service.default-jms-host=jmsuniversite
