72ee97605a9d870f173c2724793212d1a6bedb84
mh0 <mahdi.hentati@gmail.com>
2018-11-18 17:39:23 +0100

    maj

--- DptEnseignement/DptEnseignement-ejb/src/main/java/controllers/PreconventionController.java
+++ DptEnseignement/DptEnseignement-ejb/src/main/java/controllers/PreconventionController.java
@@ -30,12 +30,12 @@ public class PreconventionController implements PreconventionControllerRemote {
     // TODO: Tester l'injection
     @EJB
     ValidationPedagogiqueSender vpSender;
-    
+
     @EJB
     DepartementRepositoryLocal dprepo;
 
     @Override
-    public Map<Long, DemandePedagogique> recupererPreconventionsEnCours() {
+    public Map<Long, DemandePedagogique> recupererPreconventionsEnCours(Long dptKey) {
         // APPEL JMS POUR RECUPERATION DE NOUVELLES PRECONV
 
         // STOCKAGE EN MEMOIRE DE CES NOUVELLES PRECONV
@@ -44,6 +44,11 @@ public class PreconventionController implements PreconventionControllerRemote {
 
         // RENVOI DE LA LISTE DES PRECONV
         return listePEC;
+    }
+
+    @Override
+    public Map<Long, DemandePedagogique> recupererPreconventionsValides(Long dptKey) {
+        return this.repo.getAllPreconventionsValides();
     }
 
     /*
@@ -97,8 +102,4 @@ public class PreconventionController implements PreconventionControllerRemote {
         return dprepo.obtenirDiplomes(d);
     }
 
-    @Override
-    public Map<Long, DemandePedagogique> recupererPreconventionsValides() {
-        return this.repo.getAllPreconventionsValides();
-    }
 }
